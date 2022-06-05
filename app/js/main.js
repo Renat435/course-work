87b4acf3cf5b9e9ac1e248144d73b3cd43c43e2e
Renat <74069993+Renat435@users.noreply.github.com>
2022-06-05 17:56:54 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -2,18 +2,56 @@ $('.main__filtration-inner').click(function () {
     $(this).next('.main__filtration-hidden').toggleClass('active');
 });
 
+$('.reset-button').click(function () {
+    $(this)
+        .closest('.main__filtration-hidden')
+        .find('.main__filtration-hidden-list-label-hidden')
+        .prop('checked', false);
+});
 
-ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [55.76, 37.64],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 7
-        });
+let placemarks = [];
+let geoObjects = [];
+
+
+function init(){
+    let map = new ymaps.Map('map', {
+        center: [43.026060, 44.691290],
+        zoom: 13,
+        controls: ['zoomControl'],
+    });
+    for (let index = 0; index < placemarks.length; index++) {
+        const item = placemarks[index];
+        geoObjects[index] = new ymaps.Placemark([item.latitude, item.longitude], {
+            balloonContentHeader: "Балун метки",
+            balloonContentBody: "Содержимое <em>балуна</em> метки",
+            balloonContentFooter: "Подвал",
+            hintContent: "Согу",
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: '../img/map-placemark.png',
+            iconImageSeize: [46, 57],
+            iconImageOffset: [-23, -20],
+        })
     }
+    let clusterer = new ymaps.Clusterer({});
+    map.geoObjects.add(clusterer);
+    clusterer.add(geoObjects);
+    $('.main__block-button').click(function() {
+        map.setZoom(16);
+        map.panTo([$(this).data('latitude'), $(this).data('longitude')], {flying: 1});
+    });
+}
+
+function placemarksInit() {
+    geoObjects = [];
+    placemarks = [];
+    $('.main__block-button').each(function(index, item) {
+        placemarks.push({
+            latitude: $(this).data('latitude'),
+            longitude: $(this).data('longitude'),
+        })
+    })
+    init();
+}
+
+ymaps.ready(placemarksInit);
